5563dfeb3fea69227ee87274a338a6423b1dcfbf
samadasyed <samadasyed@hotmail.com>
2026-02-11 13:37:19 -0600

    Finished running and training

--- perceptron.py
+++ perceptron.py
@@ -10,16 +10,17 @@ class Perceptron:
         self.weights = np.zeros(input_size)
         self.bias = 0
 
-        # Forward Propagation
+    # Forward Propagation
     def forward(self, input):
         layer_output = np.dot(input, self.weights) + self.bias
         return np.where(layer_output > 0, 1, 0)
-        # Back Propagation
+
+    # Back Propagation
     def backward(self, error, input):
         self.weights += self.learning_rate * error * np.array(input)
         self.bias += self.learning_rate * error
 
-        # Train
+    # Train
     def train(self, inputs, labels):
         # Iterate Epochs
         for _ in range(self.num_epochs):
@@ -27,10 +28,10 @@ class Perceptron:
             for input, label in zip(inputs, labels):
                 # Predict
                 prediction = self.forward(input)
-                
+
                 # Back Propogation
                 self.backward(label - prediction, input)
-    
+
     # Test
     def test(self, inputs, labels):
         # Iterate Pairs of Inputs and Labels
@@ -41,15 +42,15 @@ class Perceptron:
             # Print
             print(f'Input: {input}, Prediction: {prediction}, Label: {label}')
 
-    # Initialize Perceptron
-    perceptron = Perceptron(input_size = 2, num_epochs = 1_000, learning_rate = 0.01)
+# Initialize Perceptron
+perceptron = Perceptron(input_size = 2, num_epochs = 1_000, learning_rate = 0.01)
 
-    ##### Training #####
-    training_inputs = [[1, 1], [1, 0], [0, 1], [0, 0]]
-    training_labels = [1, 0, 0, 0]
-    perceptron.train(training_inputs, training_labels)
+##### Training #####
+training_inputs = [[1, 1], [1, 0], [0, 1], [0, 0]]
+training_labels = [1, 0, 0, 0]
+perceptron.train(training_inputs, training_labels)
 
-    ##### Testing #####
-    testing_inputs = [[1, 1], [0, 1]]
-    testing_labels = [1, 0]
-    perceptron.test(testing_inputs, testing_labels)        
+##### Testing #####
+testing_inputs = [[1, 1], [0, 1]]
+testing_labels = [1, 0]
+perceptron.test(testing_inputs, testing_labels)
